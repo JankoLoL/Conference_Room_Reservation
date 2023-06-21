d38709d7f0697338f91f1b943d05e63e17a77666
JankoLoL <adam.chch86@gmail.com>
2023-06-21 10:01:11 +0200

    update

--- Conference_Room_project/settings.py
+++ Conference_Room_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p%ayn7r01i)*sdsd+cvyw6*n+47o^pj=!xenkj^*t0ps7@9bf!'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Conference_Room_app',
 ]
 
 MIDDLEWARE = [
